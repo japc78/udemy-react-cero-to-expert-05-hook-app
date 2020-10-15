2bcf5d4a0258bd30acd32385f55eada19313eefe
japc <japc.grafico@gmail.com>
2020-10-15 10:58:38 +0200

    CounterWithCustomHooks component Custom HooK contador y uso del useState

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+import { CounterWithCustomHooks } from './components/01-useState/CounterWithCustomHooks';
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <CounterApp />,
+  <CounterWithCustomHooks />,
   document.getElementById('root')
 );
