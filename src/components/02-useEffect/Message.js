4c694b198ee7249a0279df65b736ea35645f4dca
japc <japc.grafico@gmail.com>
2020-10-15 12:56:25 +0200

    UseEffect - importante Precauciones al desmontar el elemento y evitar un comportamiento no deseado de los efectos

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,18 +1,39 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
+
+	const [coords, setCoords] = useState({ x:0, y:0 })
+	const { x, y } = coords;
 
 	useEffect(() => {
 		console.log("componente montado");
 
+		const mouseMove = (e) => {
+			// console.log(e);
+			const coords = {
+				x: e.x,
+				y: e.y
+			}
+
+			setCoords(coords);
+			// console.log(":D");
+		}
+
+		// Capturamos el evento del movimiento del raton
+		window.addEventListener('mousemove', mouseMove);
+
 		return () => {
 			console.log("componente desmontado");
+			window.removeEventListener('mousemove', mouseMove)
 		}
 	}, [])
 
 	return (
 		<div>
 			<h3>Eres genial!</h3>
+			<p>
+				x: {x}, y: {y}
+			</p>
 		</div>
 	)
 }
