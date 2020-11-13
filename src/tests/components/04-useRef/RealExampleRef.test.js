0a00c9d825546acfa800c56e959fea0f58e6fe26
japc <japc.grafico@gmail.com>
2020-11-13 14:47:34 +0100

    Fix. Delete import no used MultipleCustomHooks

--- src/tests/components/04-useRef/RealExampleRef.test.js
+++ src/tests/components/04-useRef/RealExampleRef.test.js
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom'
 import { shallow } from 'enzyme';
-import { MultipleCustomHooks } from '../../../components/03-examples/MultipleCustomHooks';
 import { RealExampleRef } from '../../../components/04-useRef/RealExampleRef';
 
 describe('Test interactions with the useState', () => {
